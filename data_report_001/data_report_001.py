b0a95914e7bbb5d3fccfd28250c3b64defd8e175
AlfonsoAreizaG <63620799+Alfareiza@users.noreply.github.com>
2022-01-24 18:25:17 -0300

    Create data_report_001.py

---
+++ data_report_001/data_report_001.py
@@ -0,0 +1,41 @@
+PASTA = 'report_001'
+NOME_EMPRESA = "Minha Empresa de Midia"
+NOME_FANTASIA = "FORÇA MIDIA"
+CNPJ_EMPRESA = "XX.XXX.XXX/XXXX-XX "
+ENDERECO_EMPRESA = "Av. Alfonso Pena, 1500, Xº andar - \nCentro - Cidade/MG"
+TELEFONE_EMPRESA = "+55 11 9 5987-2598"
+CLIENTE = "NOME DO CLIENTE"
+DATA_INICIAL = "20-12-2021"
+DATA_FINAL = "31-12-2021"
+CLIENTES = {'cliente_1': {
+        'conteudos': [571, 570],
+        'players': [
+                    {'id': 3, 'endereco': 'Endereço do Player', 'insercoes': 700},
+                    {'id': 7, 'endereco': 'Endereço do Player', 'insercoes': 700},
+                    {'id': 8, 'endereco': 'Endereço do Player', 'insercoes': 700},
+                    {'id': 16, 'endereco': 'Endereço do Player', 'insercoes': 700},
+                    {'id': 17, 'endereco': 'Endereço do Player', 'insercoes': 700}
+                    ],
+                },
+    'cliente_2': {
+        'conteudos': [3351, 3352],
+        'players': [
+                    {'id': 2, 'endereco': 'Endereço do Player', 'insercoes': 720},
+                    {'id': 3, 'endereco': 'Endereço do Player', 'insercoes': 720},
+                    {'id': 4, 'endereco': 'Endereço do Player', 'insercoes': 720},
+                    {'id': 5, 'endereco': 'Endereço do Player', 'insercoes': 720},
+                    {'id': 48, 'endereco': 'Endereço do Player', 'insercoes': 720},
+                    {'id': 51, 'endereco': 'Endereço do Player', 'insercoes': 720}
+                    ],
+                },
+    'cliente_3': {
+        'conteudos': [4762, 4763],
+        'players': [
+                    {'id': 19, 'endereco': 'Endereço do Player', 'insercoes': 540},
+                    {'id': 22, 'endereco': 'Endereço do Player', 'insercoes': 540},
+                    {'id': 25, 'endereco': 'Endereço do Player', 'insercoes': 540},
+                    {'id': 26, 'endereco': 'Endereço do Player', 'insercoes': 540},
+                    {'id': 29, 'endereco': 'Endereço do Player', 'insercoes': 540},
+                    ],
+    }
+}
